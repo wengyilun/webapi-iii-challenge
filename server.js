25d0a83c8c9f733807aa34a71dca6b7b6a871495
YIlun Weng <yilunweng@YIluns-iMac-Pro.local>
2019-04-24 23:58:59 -0700

    Add uppercase middleware

--- server.js
+++ server.js
@@ -3,21 +3,36 @@ const userRoute = require('./routers/usersRouter')
 const postRoute = require('./routers/postsRouter')
 const cors = require('cors')
 const express = require('express')
-// const helmet = require('helmet')
-// const morgan = require('morgan')
+const helmet = require('helmet')
+const morgan = require('morgan')
 
 const server = express()
 
 server.use(express.json())
-
-server.use('/api/users', userRoute)
+server.use(helmet())
+server.use(morgan())
+server.use('/api/users', toUppercase(), userRoute)
 server.use('/api/posts', postRoute)
 
-// server.use(helmet())
-// server.use(morgan())
+function toUppercase(){
+	return function (req, res, next){
+		if(req.method === "POST"){
+			console.log('req.')
+			req.body.name = req.body.name.toUpperCase()
+		}
+		next()
+	}
+}
 
 server.get('/', (req, res)=>{
 	res.status(200).end('Welcome')
 })
+
+
+function errorHandler( err, req, res, next){
+	res.status(400).send("Bad Panda")
+}
+
+
 
 module.exports = server
